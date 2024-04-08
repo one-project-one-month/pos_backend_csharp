3b8b6191e58b2141cb5a7a36f518c1eb57a06c56
yoonshweyi2580 <yoonshweyi2580@gmail.com>
2024-04-08 09:36:36 +0630

    [Complete] CRUD Staff

--- DotNet7.PosBackendApi/DotNet7.PosBackendApi/Features/Setup/Staff/StaffController.cs
+++ DotNet7.PosBackendApi/DotNet7.PosBackendApi/Features/Setup/Staff/StaffController.cs
@@ -1,24 +1,24 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DotNet7.PosBackendApi.Models.Setup.Staff;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNet7.PosBackendApi.Features.Setup.Staff
 {
-    [Route("api/v1/[controller]")]
+   [Route("api/v1/[controller]")]
     [ApiController]
-    public class StaffController : BaseController
+   public class StaffController : BaseController
     {
-        private readonly StaffService _staffService;
+       private readonly StaffService _staffService;
 
-        public StaffController(StaffService staffService)
-        {
-            _staffService = staffService;
+       public StaffController(StaffService staffService)
+       {
+           _staffService = staffService;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetStaffs()
-        {
-            try
-            {
+      {
+           try{
                 return Ok(await _staffService.GetStaffs());
             }
             catch (Exception ex)
@@ -26,5 +26,86 @@ namespace DotNet7.PosBackendApi.Features.Setup.Staff
                 return InternalServerError(ex);
             }
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetStaff(int id)
+         {
+            try
+                {
+                   var staff = await _staffService.GetStaff(id);
+                    if (staff == null)
+                     {
+                        return NotFound();
+                     }
+                        return Ok(staff);
+                    }
+                    catch (Exception ex)
+                    {
+                        return InternalServerError(ex);
+                 }
+           }
+
+        [HttpPost]
+
+       public async Task<IActionResult> Create([FromBody] StaffModel staffmodel)
+         {
+            try
+               {
+                 if (staffmodel == null)
+                    {
+                       return BadRequest("Staff is null");
+                    }
+                    await _staffService.AddStaff(staffmodel);
+                    return CreatedAtAction(nameof(GetStaff), new { id = staffmodel.StaffId }, staffmodel);
+                    }
+             catch (Exception ex)
+             {
+                return InternalServerError(ex);
+             }
+          }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] StaffModel staff)
+        {
+
+            var existingUser = await _staffService.GetStaff(id);
+            if (existingUser == null)
+            {
+                return NotFound();
+            }
+
+            existingUser.StaffName = staff.StaffName;
+            existingUser.StaffCode = staff.StaffCode;
+            existingUser.DateOfBirth = staff.DateOfBirth;
+            existingUser.Address = staff.Address;
+            existingUser.Gender = staff.Gender;
+            existingUser.Position = staff.Position;
+            existingUser.MobileNo = staff.MobileNo;
+
+            await _staffService.UpdateStaff(existingUser);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var staff= await _staffService.GetStaff(id);
+                if (staff == null)
+                {
+                    return NotFound();
+                }
+                await _staffService.DeleteStaff(id);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
     }
 }
--- DotNet7.PosBackendApi/DotNet7.PosBackendApi/Features/Setup/Staff/StaffService.cs
+++ DotNet7.PosBackendApi/DotNet7.PosBackendApi/Features/Setup/Staff/StaffService.cs
@@ -1,6 +1,7 @@
 ﻿using DotNet7.PosBackendApi.DbService.DbModels;
 using DotNet7.PosBackendApi.Models.Setup.Staff;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace DotNet7.PosBackendApi.Features.Setup.Staff
 {
@@ -27,6 +28,70 @@ namespace DotNet7.PosBackendApi.Features.Setup.Staff
                 StaffId = x.StaffId,
                 StaffName = x.StaffName,
             }).ToList();
+        }
+
+        public async Task<StaffModel> GetStaff(int id)
+        {
+            // var lst = await _context.TblStaffs.FindAsync(id);
+            var lst = await _context.TblStaffs.FirstOrDefaultAsync(x => x.StaffId == id);
+            var StaffModel = new StaffModel
+            {
+                Address = lst.Address,
+                DateOfBirth = lst.DateOfBirth,
+                Gender = lst.Gender,
+                MobileNo = lst.MobileNo,
+                Position = lst.Position,
+                StaffCode = lst.StaffCode,
+                StaffId = lst.StaffId,
+                StaffName = lst.StaffName
+            };
+            return StaffModel;
+        }
+
+        public async Task AddStaff(StaffModel staffmodel)
+        {
+            TblStaff model = new TblStaff()
+            {
+                Address = staffmodel.Address,
+                DateOfBirth = staffmodel.DateOfBirth,
+                Gender = staffmodel.Gender,
+                MobileNo = staffmodel.MobileNo,
+                Position = staffmodel.Position,
+                StaffCode = staffmodel.StaffCode,
+                StaffId = staffmodel.StaffId,
+                StaffName = staffmodel.StaffName
+            };
+          
+            await _context.TblStaffs.AddAsync(model);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateStaff(StaffModel staffmodel)
+        {
+          
+            TblStaff staff = await _context.TblStaffs.FindAsync(staffmodel.StaffId);
+
+            staff.Address = staffmodel.Address;
+            staff.DateOfBirth = staffmodel.DateOfBirth;
+            staff.Gender = staffmodel.Gender;
+            staff.MobileNo = staffmodel.MobileNo;
+            staff.Position = staffmodel.Position;
+            staff.StaffCode = staffmodel.StaffCode;
+            staff.StaffName = staffmodel.StaffName;
+
+            _context.TblStaffs.Update(staff);
+            await _context.SaveChangesAsync();
+        }
+
+
+        public async Task DeleteStaff(int id)
+       {
+            TblStaff staff = await _context.TblStaffs.FindAsync(id);
+            if (staff != null)
+            {
+                _context.TblStaffs.Remove(staff);
+                await _context.SaveChangesAsync();
+            }
+
         }
     }
 }
