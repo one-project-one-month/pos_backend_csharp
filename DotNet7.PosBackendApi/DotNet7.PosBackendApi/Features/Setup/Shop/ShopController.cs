93b4cabd8841481e2c2f7ffd7a3f5a070cd2c475
Zin Linn Htet <mackk5504@gmail.com>
2024-04-09 13:44:27 +0630

    [Add] Global Using

--- DotNet7.PosBackendApi/DotNet7.PosBackendApi/Features/Setup/Shop/ShopController.cs
+++ DotNet7.PosBackendApi/DotNet7.PosBackendApi/Features/Setup/Shop/ShopController.cs
@@ -1,111 +1,101 @@
-﻿using DotNet7.PosBackendApi.Features.Setup.Staff;
-using DotNet7.PosBackendApi.Models;
-using DotNet7.PosBackendApi.Models.Setup.Shop;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System.Runtime.InteropServices.JavaScript;
+﻿namespace DotNet7.PosBackendApi.Features.Setup.Shop;
 
-namespace DotNet7.PosBackendApi.Features.Setup.Shop
+[Route("api/v1/shops")]
+[ApiController]
+public class ShopController : BaseController
 {
-    [Route("api/v1/shops")]
-    [ApiController]
-    public class ShopController : BaseController
+    private readonly ShopService _shopService;
+    private readonly BL_Shop _bL_Shop;
+    private readonly ResponseModel _response;
+
+    public ShopController(ShopService shopService, BL_Shop bL_Shop, ResponseModel response)
     {
-        private readonly ShopService _shopService;
-        private readonly BL_Shop _bL_Shop;
-        private readonly ResponseModel _response;
+        _shopService = shopService;
+        _bL_Shop = bL_Shop;
+        _response = response;
+    }
 
-        public ShopController(ShopService shopService, BL_Shop bL_Shop, ResponseModel response)
+    [HttpGet]
+    public async Task<IActionResult> GetShops()
+    {
+        try
         {
-            _shopService = shopService;
-            _bL_Shop = bL_Shop;
-            _response = response;
+            var shopLst =  await _bL_Shop.GetShops();
+            var responseModel = _response.ReturnGet(shopLst.Count,shopLst);
+            return Ok(responseModel);
         }
+        catch (Exception ex)
+        {
+            return InternalServerError(ex);
+        }
+    }
 
-        [HttpGet]
-        public async Task<IActionResult> GetShops()
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetShop(int id)
+    {
+        try
         {
-            try
-            {
-                var shopLst =  await _bL_Shop.GetShops();
-                var responseModel = _response.ReturnGet(shopLst.Count,shopLst);
-                return Ok(responseModel);
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            var shop = await _bL_Shop.GetShop(id);
+            var responseModel = _response.ReturnById(shop);
+            return Ok(responseModel);
+            //return Ok(await _shopService.GetShop(id));
         }
+        catch (Exception ex)
+        {
+            return InternalServerError(ex);
+        }
+    }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetShop(int id)
+    [HttpPost]
+    public async Task<IActionResult> CreateShop(ShopModel shop)
+    {
+        try
         {
-            try
-            {
-                var shop = await _bL_Shop.GetShop(id);
-                var responseModel = _response.ReturnById(shop);
-                return Ok(responseModel);
-                //return Ok(await _shopService.GetShop(id));
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            JObject jObject = new JObject();
+            jObject.Add("shop", "valueqwerqwer");
+            var model = await _bL_Shop.CreateShop(shop);
+            //var responseModel = _response.ReturnCommand(model.IsSuccess,model.Message,shop);
+            var responseModel = _response.ReturnCommandV1(model.IsSuccess, model.Message,"shop" ,shop);
+            return Content(JsonConvert.SerializeObject(responseModel),"application/json");
+            //return Ok(await _shopService.CreateShop(shop));
         }
+        catch (Exception ex)
+        {
+            return InternalServerError(ex);
+        };
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateShop(ShopModel shop)
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateShop(int id, ShopModel shop)
+    {
+        try
         {
-            try
-            {
-                JObject jObject = new JObject();
-                jObject.Add("shop", "valueqwerqwer");
-                var model = await _bL_Shop.CreateShop(shop);
-                //var responseModel = _response.ReturnCommand(model.IsSuccess,model.Message,shop);
-                var responseModel = _response.ReturnCommandV1(model.IsSuccess, model.Message,"shop" ,shop);
-                return Content(JsonConvert.SerializeObject(responseModel),"application/json");
-                //return Ok(await _shopService.CreateShop(shop));
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            };
+            var model = await _bL_Shop.UpdateShop(id, shop);
+            var responseModel = _response.ReturnCommand(model.IsSuccess, model.Message, shop);
+            return Ok(responseModel);
+            //return Ok(await _shopService.UpdateShop(id, shop));
         }
+        catch (Exception ex)
+        {
+            return InternalServerError(ex);
+        };
+    }
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateShop(int id, ShopModel shop)
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteShop(int id)
+    {
+        try
         {
-            try
-            {
-                var model = await _bL_Shop.UpdateShop(id, shop);
-                var responseModel = _response.ReturnCommand(model.IsSuccess, model.Message, shop);
-                return Ok(responseModel);
-                //return Ok(await _shopService.UpdateShop(id, shop));
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            };
+            JObject jObject =  new JObject();
+            jObject.Add("shop","valueqwerqwer");
+            var model = await _bL_Shop.DeleteShop(id);
+            var responseModel = _response.ReturnCommand(model.IsSuccess, model.Message, jObject:jObject);
+            return Ok(responseModel);
+            // return Ok(await _shopService.DeleteShop(id));
         }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteShop(int id)
+        catch (Exception ex)
         {
-            try
-            {
-                JObject jObject =  new JObject();
-                jObject.Add("shop","valueqwerqwer");
-                var model = await _bL_Shop.DeleteShop(id);
-                var responseModel = _response.ReturnCommand(model.IsSuccess, model.Message, jObject:jObject);
-                return Ok(responseModel);
-               // return Ok(await _shopService.DeleteShop(id));
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            };
-        }
+            return InternalServerError(ex);
+        };
     }
 }
