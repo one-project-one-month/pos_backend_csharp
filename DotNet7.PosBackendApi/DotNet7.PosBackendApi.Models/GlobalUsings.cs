0936f70a608288fb2d9a7512b16ab52d1af702f7
Zin Linn Htet <mackk5504@gmail.com>
2024-04-09 13:43:59 +0630

    [Add] Global Using

---
+++ DotNet7.PosBackendApi/DotNet7.PosBackendApi.Models/GlobalUsings.cs
@@ -0,0 +1,5 @@
+// Global using directives
+
+global using DotNet7.PosBackendApi.DbService.DbModels;
+global using DotNet7.PosBackendApi.Models.Setup.Shop;
+global using DotNet7.PosBackendApi.Models.Setup.Staff;
---
+++ DotNet7.PosBackendApi/DotNet7.PosBackendApi/GlobalUsings.cs
@@ -0,0 +1,12 @@
+// Global using directives
+
+global using DotNet7.PosBackendApi.DbService.DbModels;
+global using DotNet7.PosBackendApi.Models;
+global using DotNet7.PosBackendApi.Models.Setup.Shop;
+global using DotNet7.PosBackendApi.Models.Setup.Staff;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+global using Newtonsoft.Json;
+global using Newtonsoft.Json.Linq;
+global using DotNet7.PosBackendApi.Features.Setup.Shop;
+global using DotNet7.PosBackendApi.Features.Setup.Staff;
