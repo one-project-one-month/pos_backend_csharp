5528fff63e38d6cd540ae18ed6561058c75d56ea
Zin Linn Htet <mackk5504@gmail.com>
2024-04-09 13:43:16 +0630

    [Add] Staff Change Model

--- DotNet7.PosBackendApi/DotNet7.PosBackendApi.Models/ChangeModel.cs
+++ DotNet7.PosBackendApi/DotNet7.PosBackendApi.Models/ChangeModel.cs
@@ -1,38 +1,68 @@
-﻿using DotNet7.PosBackendApi.DbService.DbModels;
-using DotNet7.PosBackendApi.Models.Setup.Shop;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet7.PosBackendApi.Models;
 
-namespace DotNet7.PosBackendApi.Models
+public static class ChangeModel
 {
-    public static class ChangeModel
+    #region Shop
+
+    public static TblShop Change(this ShopModel requestModel)
     {
-        public static TblShop Change(this ShopModel requestModel)
+        var shopModel = new TblShop()
         {
-            var shopModel = new TblShop()
-            {
-                //ShopId = shop.ShopId,
-                ShopCode = requestModel.ShopCode,
-                ShopName = requestModel.ShopName,
-                MobileNo = requestModel.MobileNo,
-                Address = requestModel.Address
-            };
-            return shopModel;
-        }
-        public static ShopModel Change(this TblShop dataModel)
-        {
-            var shopModel = new ShopModel()
-            {
-                ShopId = dataModel.ShopId,
-                ShopCode = dataModel.ShopCode,
-                ShopName = dataModel.ShopName,
-                MobileNo = dataModel.MobileNo,
-                Address = dataModel.Address
-            };
-            return shopModel;
-        }
+            //ShopId = shop.ShopId,
+            ShopCode = requestModel.ShopCode,
+            ShopName = requestModel.ShopName,
+            MobileNo = requestModel.MobileNo,
+            Address = requestModel.Address
+        };
+        return shopModel;
     }
+
+    public static ShopModel Change(this TblShop dataModel)
+    {
+        var shopModel = new ShopModel()
+        {
+            ShopId = dataModel.ShopId,
+            ShopCode = dataModel.ShopCode,
+            ShopName = dataModel.ShopName,
+            MobileNo = dataModel.MobileNo,
+            Address = dataModel.Address
+        };
+        return shopModel;
+    }
+
+    #endregion
+
+    #region Staff
+
+    public static StaffModel Change(this TblStaff dataModel)
+    {
+        var staffModel = new StaffModel()
+        {
+            StaffId = dataModel.StaffId,
+            StaffCode = dataModel.StaffCode,
+            DateOfBirth = dataModel.DateOfBirth,
+            MobileNo = dataModel.MobileNo,
+            Address = dataModel.Address,
+            Gender = dataModel.Gender,
+            Position = dataModel.Gender
+        };
+        return staffModel;
+    }
+
+    public static TblStaff Change(this StaffModel requestModel)
+    {
+        var staffModel = new TblStaff()
+        {
+            // StaffId = requestModel.StaffId,
+            StaffCode = requestModel.StaffCode,
+            DateOfBirth = requestModel.DateOfBirth,
+            MobileNo = requestModel.MobileNo,
+            Address = requestModel.Address,
+            Gender = requestModel.Gender,
+            Position = requestModel.Gender
+        };
+        return staffModel;
+    }
+
+    #endregion
 }
