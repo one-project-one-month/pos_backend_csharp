69ecddae9b61945460389975011dea2697827a91
Zin Linn Htet <mackk5504@gmail.com>
2024-04-10 14:55:42 +0630

    Complete staff list response model

---
+++ DotNet7.PosBackendApi/DotNet7.PosBackendApi.Models/Setup/Staff/StaffListResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DotNet7.PosBackendApi.Models.Setup.Staff;
+
+public class StaffListResponseModel
+{
+    public List<StaffModel> DataList { get; set; }
+    public MessageResponseModel MessageResponse { get; set; }
+}
