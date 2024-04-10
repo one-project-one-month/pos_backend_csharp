85fb460182b0deb4e46b8800fa0efde6ccc94815
Zin Linn Htet <mackk5504@gmail.com>
2024-04-10 14:54:44 +0630

    [Complete] Shop Model

--- DotNet7.PosBackendApi/DotNet7.PosBackendApi.Models/Setup/Shop/ShopModel.cs
+++ DotNet7.PosBackendApi/DotNet7.PosBackendApi.Models/Setup/Shop/ShopModel.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet7.PosBackendApi.Models.Setup.Shop;
 
-namespace DotNet7.PosBackendApi.Models.Setup.Shop
+public class ShopModel
 {
-    public class ShopModel
-    {
-        public int ShopId { get; set; }
+    public int ShopId { get; set; }
 
-        public string ShopCode { get; set; } = null!;
+    public string ShopCode { get; set; } = null!;
 
-        public string ShopName { get; set; } = null!;
+    public string ShopName { get; set; } = null!;
 
-        public string MobileNo { get; set; } = null!;   
+    public string MobileNo { get; set; } = null!;   
 
-        public string Address { get; set; } = null!;
-    }
+    public string Address { get; set; } = null!;
 }
