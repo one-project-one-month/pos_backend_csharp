8d8006aad7296c29fc30fc19c7641461daa997a1
Zin Linn Htet <mackk5504@gmail.com>
2024-04-10 14:53:58 +0630

    [Complete] Message Response Model

--- DotNet7.PosBackendApi/DotNet7.PosBackendApi.Models/MessageResponseModel.cs
+++ DotNet7.PosBackendApi/DotNet7.PosBackendApi.Models/MessageResponseModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet7.PosBackendApi.Models;
+﻿namespace DotNet7.PosBackendApi.Models;
 public class MessageResponseModel
 {
     public MessageResponseModel() { }
