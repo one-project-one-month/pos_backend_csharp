1f15aa64b5495b9e93ee026b314f0bfc9abed93f
Zin Linn Htet <mackk5504@gmail.com>
2024-04-10 14:52:44 +0630

    Change File scopes

--- DotNet7.PosBackendApi/DotNet7.PosBackendApi.Models/EnumStatus.cs
+++ DotNet7.PosBackendApi/DotNet7.PosBackendApi.Models/EnumStatus.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet7.PosBackendApi.Models;
 
-namespace DotNet7.PosBackendApi.Models
+public enum EnumStatus
 {
-    public enum EnumStatus
-    {
-        Success,
-        Fail,
-        NotFound
-    }
+    Success,
+    Fail,
+    NotFound
 }
