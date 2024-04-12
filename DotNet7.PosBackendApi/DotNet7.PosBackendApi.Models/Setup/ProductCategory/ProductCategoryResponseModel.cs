50a501899ca192a7217badb7cebdfebe2c7e30fe
Zin Linn Htet <mackk5504@gmail.com>
2024-04-13 01:12:08 +0630

    Add Product Category Model

---
+++ DotNet7.PosBackendApi/DotNet7.PosBackendApi.Models/Setup/ProductCategory/ProductCategoryResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNet7.PosBackendApi.Models.Setup.ProductCategory;
+
+public class ProductCategoryResponseModel
+{
+    public ProductCategoryModel Data { get; set; }
+    public MessageResponseModel MessageResponse { get; set; }
+
+
+}
