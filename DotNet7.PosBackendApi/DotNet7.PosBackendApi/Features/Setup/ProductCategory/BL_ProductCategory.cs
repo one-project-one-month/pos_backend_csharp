a4331ac3834d8f36b1da7a2e62e2828a9c4db493
Zin Linn Htet <mackk5504@gmail.com>
2024-04-13 01:12:40 +0630

    Complete BL Product Category Model

---
+++ DotNet7.PosBackendApi/DotNet7.PosBackendApi/Features/Setup/ProductCategory/BL_ProductCategory.cs
@@ -0,0 +1,62 @@
+﻿namespace DotNet7.PosBackendApi.Features.Setup.ProductCategory;
+
+public class BL_ProductCategory
+{
+    private readonly DL_ProductCategory _productCategory;
+
+    public BL_ProductCategory(DL_ProductCategory productCategory)
+    {
+        _productCategory = productCategory;
+    }
+
+    public async Task<ProductCategoryListResponseModel> GetProductCategory()
+    {
+        var response = await _productCategory.GetProductCategory();
+        return response;
+    }
+
+    public async Task<ProductCategoryResponseModel> GetProductCategoryByCode(string productCategoryCode)
+    {
+        if (productCategoryCode is null) throw new Exception("productCategoryCode is null");
+        var response = await _productCategory.GetProductCategoryByCode(productCategoryCode);
+        return response;
+    }
+
+    public async Task<MessageResponseModel> CreateProductCategory(ProductCategoryModel requestModel)
+    {
+        CheckProductNullValue(requestModel);
+        var response = await _productCategory.CreateProductCategory(requestModel);
+        return response;
+    }
+
+    public async Task<MessageResponseModel> UpdateProductCategory(int id, ProductCategoryModel requestModel)
+    {
+        if (id <= 0) throw new Exception("productCategoryCode is null");
+        CheckProductNullValue(requestModel);
+        var response = await _productCategory.UpdateProductCategory(id, requestModel);
+        return response;
+    }
+
+    public async Task<MessageResponseModel> DeleteProductCategory(int id)
+    {
+        if (id <= 0) throw new Exception("productCategoryId is null");
+        var response = await _productCategory.DeleteProductCategory(id);
+        return response;
+    }
+
+    private static void CheckProductNullValue(ProductCategoryModel productCategory)
+    {
+        if (productCategory == null)
+        {
+            throw new Exception("productCategory is null.");
+        }
+        if (string.IsNullOrWhiteSpace(productCategory.ProductCategoryName))
+        {
+            throw new Exception("ProductCategoryName is null.");
+        }
+        if (string.IsNullOrWhiteSpace(productCategory.ProductCategoryCode))
+        {
+            throw new Exception("ProductCategoryCode is null.");
+        }
+    }
+}
