e74104d6e13a0618593772a2855c74df2030fa8d
Zin Linn Htet <mackk5504@gmail.com>
2024-04-18 22:18:10 +0630

    [Complete] Staff Service

---
+++ DotNet7.PosBackendApi/DotNet8.PosBackendApi.Models/Setup/Staff/StaffResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DotNet8.PosBackendApi.Models.Setup.Staff;
+
+public class StaffResponseModel
+{
+    public StaffModel Data { get; set; }
+    public MessageResponseModel MessageResponse { get; set; }
+}
---
+++ DotNet7.PosBackendApi/DotNet8.PosBackendApi/Features/Setup/Staff/StaffService.cs
@@ -0,0 +1,95 @@
+﻿using DotNet8.PosBackendApi.DbService.Models;
+using DotNet8.PosBackendApi.Models.Setup.Staff;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8.PosBackendApi.Features.Setup.Staff;
+
+public class StaffService
+{
+    private readonly AppDbContext _context;
+
+    public StaffService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<StaffModel>> GetStaffs()
+    {
+        var lst = await _context.TblStaffs.ToListAsync();
+        return lst.Select(x => new StaffModel
+        {
+            Address = x.Address,
+            DateOfBirth = x.DateOfBirth,
+            Gender = x.Gender,
+            MobileNo = x.MobileNo,
+            Position = x.Position,
+            StaffCode = x.StaffCode,
+            StaffId = x.StaffId,
+            StaffName = x.StaffName,
+        }).ToList();
+    }
+
+    public async Task<StaffModel> GetStaff(int id)
+    {
+        // var lst = await _context.TblStaffs.FindAsync(id);
+        var lst = await _context.TblStaffs.FirstOrDefaultAsync(x => x.StaffId == id);
+        var StaffModel = new StaffModel
+        {
+            Address = lst.Address,
+            DateOfBirth = lst.DateOfBirth,
+            Gender = lst.Gender,
+            MobileNo = lst.MobileNo,
+            Position = lst.Position,
+            StaffCode = lst.StaffCode,
+            StaffId = lst.StaffId,
+            StaffName = lst.StaffName
+        };
+        return StaffModel;
+    }
+
+    public async Task AddStaff(StaffModel staffmodel)
+    {
+        TblStaff model = new TblStaff()
+        {
+            Address = staffmodel.Address,
+            DateOfBirth = staffmodel.DateOfBirth,
+            Gender = staffmodel.Gender,
+            MobileNo = staffmodel.MobileNo,
+            Position = staffmodel.Position,
+            StaffCode = staffmodel.StaffCode,
+            StaffId = staffmodel.StaffId,
+            StaffName = staffmodel.StaffName
+        };
+
+        await _context.TblStaffs.AddAsync(model);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateStaff(StaffModel staffmodel)
+    {
+
+        TblStaff staff = await _context.TblStaffs.FindAsync(staffmodel.StaffId);
+
+        staff.Address = staffmodel.Address;
+        staff.DateOfBirth = staffmodel.DateOfBirth;
+        staff.Gender = staffmodel.Gender;
+        staff.MobileNo = staffmodel.MobileNo;
+        staff.Position = staffmodel.Position;
+        staff.StaffCode = staffmodel.StaffCode;
+        staff.StaffName = staffmodel.StaffName;
+
+        _context.TblStaffs.Update(staff);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteStaff(int id)
+    {
+        TblStaff staff = await _context.TblStaffs.FindAsync(id);
+        if (staff != null)
+        {
+            _context.TblStaffs.Remove(staff);
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
