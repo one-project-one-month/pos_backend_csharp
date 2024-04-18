b60f0ae9eb7f09d21cef1f587ff0a759557e4e2d
Zin Linn Htet <mackk5504@gmail.com>
2024-04-18 22:13:29 +0630

    [Complete]ProductCategoryModel

---
+++ DotNet7.PosBackendApi/DotNet8.PosBackendApi.Models/Setup/ProductCategory/ProductCategoryModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNet8.PosBackendApi.Models.Setup.ProductCategory;
+
+public class ProductCategoryModel
+{
+    public int ProductCategoryId { get; set; }
+    public string ProductCategoryCode { get; set; } = null!;
+    public string ProductCategoryName { get; set; } = null!;
+}
