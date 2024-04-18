f1f9ce92ad26dd9a53ee3095c6f33f6872c71fd6
Zin Linn Htet <mackk5504@gmail.com>
2024-04-18 22:13:43 +0630

    [Complete] ProductCategory ResponseModel

---
+++ DotNet7.PosBackendApi/DotNet8.PosBackendApi.Models/Setup/ProductCategory/ProductCategoryResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DotNet8.PosBackendApi.Models.Setup.ProductCategory;
+
+public class ProductCategoryResponseModel
+{
+    public ProductCategoryModel Data { get; set; }
+    public MessageResponseModel MessageResponse { get; set; }
+}
