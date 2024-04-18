6f18da9f20ac32a248474fbf466ad3de7ea19f40
Zin Linn Htet <mackk5504@gmail.com>
2024-04-18 22:10:49 +0630

    [Complete] DL_Product

---
+++ DotNet7.PosBackendApi/DotNet8.PosBackendApi/Features/Setup/Product/DL_Product.cs
@@ -0,0 +1,139 @@
+﻿using DotNet8.PosBackendApi.DbService.Models;
+using DotNet8.PosBackendApi.Models;
+using DotNet8.PosBackendApi.Models.Setup.Product;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8.PosBackendApi.Features.Setup.Product;
+
+public class DL_Product
+{
+    private readonly AppDbContext _context;
+
+    public DL_Product(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ProductListResponseModel> GetProduct()
+    {
+        var responseModel = new ProductListResponseModel();
+        try
+        {
+            var products = await _context
+                .TblProducts
+                .AsNoTracking()
+                .ToListAsync();
+            responseModel.DataLst = products
+                .Select(x => x.Change())
+                .ToList();
+            responseModel.MessageResponse = new MessageResponseModel(true, EnumStatus.Success.ToString());
+        }
+        catch (Exception ex)
+        {
+            responseModel.DataLst = new List<ProductModel>();
+            responseModel.MessageResponse = new MessageResponseModel(false, ex);
+        }
+        return responseModel;
+    }
+
+    public async Task<ProductResponseModel> GetProductByCode(string productCode)
+    {
+        var responseModel = new ProductResponseModel();
+        try
+        {
+            var product = await _context
+                .TblProducts
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.ProductCode == productCode);
+            if (product is null)
+            {
+                responseModel.MessageResponse = new MessageResponseModel
+                    (false, EnumStatus.NotFound.ToString());
+                goto result;
+            }
+            responseModel.Data = product.Change();
+            responseModel.MessageResponse = new MessageResponseModel(true, EnumStatus.Success.ToString());
+        }
+        catch (Exception ex)
+        {
+            responseModel.Data = new ProductModel();
+            responseModel.MessageResponse = new MessageResponseModel(false, ex);
+        }
+    result:
+        return responseModel;
+    }
+
+    public async Task<MessageResponseModel> Create(ProductModel requestModel)
+    {
+        var responseModel = new MessageResponseModel();
+        try
+        {
+            await _context.TblProducts.AddAsync(requestModel.Change());
+            var result = await _context.SaveChangesAsync();
+            responseModel = result > 0 ?
+                new MessageResponseModel(true, EnumStatus.Success.ToString())
+                : new MessageResponseModel(false, EnumStatus.Fail.ToString());
+        }
+        catch (Exception ex)
+        {
+            responseModel = new MessageResponseModel(false, ex);
+        }
+        return responseModel;
+    }
+
+    public async Task<MessageResponseModel> Update(int id, ProductModel requestModel)
+    {
+        var responseModel = new MessageResponseModel();
+        try
+        {
+            var product = await _context
+                .TblProducts
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.ProductId == id);
+            if (product == null)
+            {
+                responseModel = new MessageResponseModel(false, EnumStatus.NotFound.ToString());
+                goto result;
+            }
+            _context.TblProducts.Update(requestModel.Change());
+            var result = await _context.SaveChangesAsync();
+            responseModel = result > 0 ?
+                new MessageResponseModel(true, EnumStatus.Success.ToString())
+                : new MessageResponseModel(false, EnumStatus.Fail.ToString());
+        }
+        catch (Exception ex)
+        {
+            responseModel = new MessageResponseModel(false, ex);
+        }
+    result:
+        return responseModel;
+    }
+
+    public async Task<MessageResponseModel> Delete(int id)
+    {
+        var responseModel = new MessageResponseModel();
+        try
+        {
+            var product = await _context
+                .TblProducts
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.ProductId == id);
+            if (product == null)
+            {
+                responseModel = new MessageResponseModel(false, EnumStatus.NotFound.ToString());
+                goto result;
+            }
+            _context.TblProducts.Remove(product);
+            var result = await _context.SaveChangesAsync();
+            responseModel = result > 0 ?
+                new MessageResponseModel(true, EnumStatus.Success.ToString())
+                : new MessageResponseModel(false, EnumStatus.Fail.ToString());
+        }
+        catch (Exception ex)
+        {
+            responseModel = new MessageResponseModel(false, ex);
+        }
+    result:
+        return responseModel;
+    }
+}
