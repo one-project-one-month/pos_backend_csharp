fae0eb5b7f364230d5455af3ed46e72d38accf0b
Zin Linn Htet <mackk5504@gmail.com>
2024-04-19 00:35:24 +0630

    Add Register Controller

---
+++ DotNet7.PosBackendApi/DotNet8.PosBackendApi/Features/Setup/Authentication/Register/RegisterController.cs
@@ -0,0 +1,25 @@
+﻿namespace DotNet8.PosBackendApi.Features.Setup.Authentication.Register;
+
+[Route("api/v1/auth/[controller]")]
+[ApiController]
+public class RegisterController : BaseController
+{
+    private readonly DL_Staff _staff;
+    private readonly ResponseModel _response;
+
+    [HttpPost]
+    public async Task<IActionResult> CreateStaff([FromBody] StaffModel requestModel)
+    {
+        try
+        {
+            var model = await _staff.CreateStaff(requestModel);
+            var responseModel = _response.ReturnCommand
+                (model.IsSuccess, model.Message, EnumPos.Staff, requestModel);
+            return Content(responseModel);
+        }
+        catch (Exception ex)
+        {
+            return InternalServerError(ex);
+        }
+    }
+}
