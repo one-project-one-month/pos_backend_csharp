aad2b412e9983438da4438a51cc48d3dfb17ce17
Zin Linn Htet <mackk5504@gmail.com>
2024-04-18 22:16:16 +0630

    [Complete] ShopList ResponseModel

---
+++ DotNet7.PosBackendApi/DotNet8.PosBackendApi.Models/Setup/Shop/ShopListResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DotNet8.PosBackendApi.Models.Setup.Shop;
+
+public class ShopListResponseModel
+{
+    public List<ShopModel> DataLst { get; set; }
+    public MessageResponseModel MessageResponse { get; set; }
+}
