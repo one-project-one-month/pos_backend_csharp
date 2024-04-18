98224fec64c0eaf2faa2840f34971a8e75e105ed
Zin Linn Htet <mackk5504@gmail.com>
2024-04-18 22:14:38 +0630

    [Complete] Product model

---
+++ DotNet7.PosBackendApi/DotNet8.PosBackendApi.Models/Setup/Product/ProductModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DotNet8.PosBackendApi.Models.Setup.Product;
+
+public class ProductModel
+{
+    public int ProductId { get; set; }
+
+    public string ProductCode { get; set; } = null!;
+
+    public string ProductCategoryCode { get; set; } = null!;
+
+    public string ProductName { get; set; } = null!;
+
+    public decimal Price { get; set; }
+}
