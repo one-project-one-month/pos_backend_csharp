c824561e044351bbd37303e80a9206a4b06b091d
Zin Linn Htet <mackk5504@gmail.com>
2024-04-18 22:16:33 +0630

    [Complete] ShopModel

---
+++ DotNet7.PosBackendApi/DotNet8.PosBackendApi.Models/Setup/Shop/ShopModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DotNet8.PosBackendApi.Models.Setup.Shop;
+
+public class ShopModel
+{
+    public int ShopId { get; set; }
+
+    public string ShopCode { get; set; } = null!;
+
+    public string ShopName { get; set; } = null!;
+
+    public string MobileNo { get; set; } = null!;   
+
+    public string Address { get; set; } = null!;
+}
