2ad5c01b9d2ea1dcb3a0b2c03fe6a3a53a61c40e
Zin Linn Htet <mackk5504@gmail.com>
2024-04-19 00:36:46 +0630

    Add token response model

---
+++ DotNet7.PosBackendApi/DotNet8.PosBackendApi.Models/Setup/Token/TokenResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DotNet8.PosBackendApi.Models.Setup.Token;
+
+public class TokenResponseModel
+{
+    public string AccessToken { get; set; }
+    public string RefreshToken { get; set; }
+}
