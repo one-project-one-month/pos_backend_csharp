fa239dabb90f8a9f377c19f446e72591947eb1c3
Zin Linn Htet <mackk5504@gmail.com>
2024-04-19 00:32:15 +0630

    Complete BL Shop

--- DotNet7.PosBackendApi/DotNet8.PosBackendApi/Features/Setup/Shop/BL_Shop.cs
+++ DotNet7.PosBackendApi/DotNet8.PosBackendApi/Features/Setup/Shop/BL_Shop.cs
@@ -1,5 +1,4 @@
-﻿using DotNet8.PosBackendApi.Models;
-using DotNet8.PosBackendApi.Models.Setup.Shop;
+﻿using DotNet8.PosBackendApi.Models.Setup.Shop;
 
 namespace DotNet8.PosBackendApi.Features.Setup.Shop;
 
