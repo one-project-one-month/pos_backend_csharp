37e37d0a31c1d521b6c543c0a8704cdff3e81cc3
Zin Linn Htet <mackk5504@gmail.com>
2024-04-18 22:12:12 +0630

    Extreact Global usings

--- DotNet7.PosBackendApi/DotNet7.PosBackendApi/GlobalUsings.cs
+++ DotNet7.PosBackendApi/DotNet7.PosBackendApi/GlobalUsings.cs
@@ -1,6 +1,8 @@
 // Global using directives
 
 global using DotNet7.PosBackendApi.DbService.Models;
+global using DotNet7.PosBackendApi.Features.Setup.Product;
+global using DotNet7.PosBackendApi.Features.Setup.ProductCategory;
 global using DotNet7.PosBackendApi.Models;
 global using DotNet7.PosBackendApi.Models.Setup.Shop;
 global using DotNet7.PosBackendApi.Models.Setup.Staff;
---
+++ DotNet7.PosBackendApi/DotNet8.PosBackendApi.Models/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿// Global using directives
+
+global using DotNet8.PosBackendApi.DbService.Models;
+global using DotNet8.PosBackendApi.Models;
+global using Newtonsoft.Json.Linq;
