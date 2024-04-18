f36659da9f19199ad4c7d175e5e4378712e9fd6d
Zin Linn Htet <mackk5504@gmail.com>
2024-04-18 22:04:48 +0630

    Set Up Project

---
+++ DotNet7.PosBackendApi/DotNet8.PosBackendApi/Features/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DotNet8.PosBackendApi.Features;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
+{
+    protected IActionResult InternalServerError(Exception ex)
+    {
+        return StatusCode(500, new
+        {
+            Message = ex.ToString()
+        });
+    }
+    protected IActionResult Content(object obj)
+    {
+        return Content(JsonConvert.SerializeObject(obj), "application/json");
+    }
+}
