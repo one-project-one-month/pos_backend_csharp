4de1d1db18157a35cd75941f8b5bf94272282527
Zin Linn Htet <mackk5504@gmail.com>
2024-04-19 00:33:44 +0630

    -

---
+++ DotNet7.PosBackendApi/DotNet8.PosBackendApi.Shared/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿// Global using directives
+
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using System.Security.Cryptography;
+global using System.Text;
+global using DotNet8.PosBackendApi.Models.Setup.Staff;
+global using Microsoft.IdentityModel.Tokens;
--- DotNet7.PosBackendApi/DotNet8.PosBackendApi/GlobalUsings.cs
+++ DotNet7.PosBackendApi/DotNet8.PosBackendApi/GlobalUsings.cs
@@ -1,4 +1,19 @@
 ﻿// Global using directives
 
+global using DotNet8.PosBackendApi;
+global using DotNet8.PosBackendApi.DbService.Models;
+global using DotNet8.PosBackendApi.Features.Setup.Product;
+global using DotNet8.PosBackendApi.Features.Setup.ProductCategory;
+global using DotNet8.PosBackendApi.Features.Setup.Shop;
+global using DotNet8.PosBackendApi.Features.Setup.Staff;
+global using DotNet8.PosBackendApi.Models;
+global using DotNet8.PosBackendApi.Models.Setup.Product;
+global using DotNet8.PosBackendApi.Models.Setup.ProductCategory;
+global using DotNet8.PosBackendApi.Models.Setup.Staff;
+global using DotNet8.PosBackendApi.Models.Setup.Token;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.IdentityModel.Tokens;
+global using Microsoft.OpenApi.Models;
 global using Newtonsoft.Json;
