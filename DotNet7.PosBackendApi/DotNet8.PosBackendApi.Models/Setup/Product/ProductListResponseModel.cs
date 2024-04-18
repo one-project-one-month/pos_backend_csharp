42e8e56ceb589e659f0ad114c6fd54bc4fb6daaa
Zin Linn Htet <mackk5504@gmail.com>
2024-04-18 22:14:22 +0630

    [Complete] Product list Response Model

---
+++ DotNet7.PosBackendApi/DotNet8.PosBackendApi.Models/Setup/Product/ProductListResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DotNet8.PosBackendApi.Models.Setup.Product;
+
+public class ProductListResponseModel
+{
+    public List<ProductModel> DataLst { get; set; }
+    public MessageResponseModel MessageResponse { get; set; }
+}
