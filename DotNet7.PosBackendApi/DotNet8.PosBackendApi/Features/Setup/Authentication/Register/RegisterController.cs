f18024c0319c98e4462a46390c2cabe82db0aba1
Zin Linn Htet <mackk5504@gmail.com>
2024-04-20 00:53:34 +0630

    Inprogress RegisterController

--- DotNet7.PosBackendApi/DotNet8.PosBackendApi/Features/Setup/Authentication/Register/RegisterController.cs
+++ DotNet7.PosBackendApi/DotNet8.PosBackendApi/Features/Setup/Authentication/Register/RegisterController.cs
@@ -6,6 +6,14 @@ public class RegisterController : BaseController
 {
     private readonly DL_Staff _staff;
     private readonly ResponseModel _response;
+    private readonly JwtTokenGenerate _token;
+
+    public RegisterController(DL_Staff staff, ResponseModel response, JwtTokenGenerate token)
+    {
+        _staff = staff;
+        _response = response;
+        _token = token;
+    }
 
     [HttpPost]
     public async Task<IActionResult> CreateStaff([FromBody] StaffModel requestModel)
@@ -15,7 +23,16 @@ public class RegisterController : BaseController
             var model = await _staff.CreateStaff(requestModel);
             //var responseModel = _response.ReturnCommand
             //    (model.IsSuccess, model.Message, EnumPos.Staff, requestModel);
-            return Content(model);
+            var responseModel = _response.Return
+            (new ReturnModel
+            {
+                Token = _token.GenerateRefreshToken(RefreshToken()),
+                EnumPos = EnumPos.Staff,
+                IsSuccess = model.IsSuccess,
+                Message = model.Message,
+                Item = requestModel
+            });
+            return Content(responseModel);
         }
         catch (Exception ex)
         {
