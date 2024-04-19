dd99fd273b561e3f01b258e28f2f060dae8de724
Zin Linn Htet <mackk5504@gmail.com>
2024-04-20 00:50:33 +0630

    Add Dev Codes

---
+++ DotNet7.PosBackendApi/DotNet8.PosBackendApi.Shared/DevCode.cs
@@ -0,0 +1,11 @@
+﻿using Effortless.Net.Encryption;
+
+namespace DotNet8.PosBackendApi.Shared;
+
+public static class DevCode
+{
+    public static string ToHash(this string password, string sharedKey)
+    {
+        return  Hash.Create(HashType.SHA256, password, sharedKey, false);
+    }
+}
