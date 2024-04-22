afb31157ba0fab3ca05a9f3497301c59239ec6fb
Zin Linn Htet <mackk5504@gmail.com>
2024-04-23 01:22:43 +0630

    Complete BL Login

---
+++ DotNet8.PosBackendApi/Features/Setup/Authentication/Login/BL_Login.cs
@@ -0,0 +1,21 @@
+﻿using DotNet8.PosBackendApi.Models.Setup.Login;
+
+namespace DotNet8.PosBackendApi.Features.Setup.Authentication.Login;
+
+public class BL_Login
+{
+    private readonly DL_Login _DL_login;
+
+    public BL_Login(DL_Login login)
+    {
+        _DL_login = login;
+    }
+
+    public async Task<TokenResponseModel> Login(LoginRequestModel reqModel)
+    {
+
+        if (reqModel is null ) throw new Exception("UserName and password cannot be null");
+        var model = await _DL_login.Login(reqModel);
+        return model;
+    }
+}
