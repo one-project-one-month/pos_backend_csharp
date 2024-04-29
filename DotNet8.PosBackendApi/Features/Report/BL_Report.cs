22ec9a35c528eb35b3412d3de08fd5a6406a72f8
suwutyeenaing@gmail.com <suwutyeenaing@gmail.com>
2024-04-29 16:17:34 +0630

    - Add Monthly Report [Done]

---
+++ DotNet8.PosBackendApi/Features/Report/BL_Report.cs
@@ -0,0 +1,20 @@
+﻿using DotNet8.PosBackendApi.Models.Setup.Report;
+
+namespace DotNet8.PosBackendApi.Features.Report
+{
+    public class BL_Report
+    {
+        private readonly DL_Report _report;
+
+        public BL_Report(DL_Report report)
+        {
+            _report = report;
+        }
+
+        public async Task<MonthlyReportResponseModel> MonthlyReport(int month, int year)
+        {
+            var response = await _report.MonthlyReport(month, year);
+            return response;
+        }
+    }
+}
