eb192d008cdc7bb0ac174de04cb30020aff54d80
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-05-01 00:36:42 +0630

    [Complete] Product Category Create

---
+++ DotNet8.PosFrontendBlazor/Services/InjectService.cs
@@ -0,0 +1,40 @@
+﻿namespace DotNet8.PosFrontendBlazor.Services;
+
+public class InjectService
+{
+    private readonly ISnackbar _snackbar;
+
+    public InjectService(ISnackbar snackbar)
+    {
+        _snackbar = snackbar;
+    }
+
+    public void ShowMessage(string message, EnumResponseType responseType)
+    {
+        switch (responseType)
+        {
+            case EnumResponseType.Success:
+                _snackbar.Add(message, Severity.Success);
+                break;
+            case EnumResponseType.Information:
+                _snackbar.Add(message, Severity.Info);
+                break;
+            case EnumResponseType.Warning:
+                _snackbar.Add(message, Severity.Warning);
+                break;
+            case EnumResponseType.Error:
+                _snackbar.Add(message, Severity.Error);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+public enum EnumResponseType
+{
+    Success,
+    Information,
+    Warning,
+    Error
+}
