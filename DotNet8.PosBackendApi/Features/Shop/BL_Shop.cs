ecc30dcad451990f671d50b3b6741254f8bee9e2
Zin Linn Htet <mackk5504@gmail.com>
2024-05-01 19:28:20 +0630

    Complete Patch Method

--- DotNet8.PosBackendApi/Features/Shop/BL_Shop.cs
+++ DotNet8.PosBackendApi/Features/Shop/BL_Shop.cs
@@ -1,15 +1,11 @@
-﻿using DotNet8.PosBackendApi.Models.Setup.Shop;
-
-namespace DotNet8.PosBackendApi.Features.Shop;
+﻿namespace DotNet8.PosBackendApi.Features.Shop;
 
 public class BL_Shop
 {
     private readonly DL_Shop _dL_Shop;
 
-    public BL_Shop(DL_Shop dL_Shop)
-    {
-        _dL_Shop = dL_Shop;
-    }
+    public BL_Shop(DL_Shop dL_Shop) => _dL_Shop = dL_Shop;
+
     public async Task<ShopListResponseModel> GetShops()
     {
         var response = await _dL_Shop.GetShops();
@@ -28,7 +24,6 @@ public class BL_Shop
         CheckShopNullValue(requestModel);
         var response = await _dL_Shop.CreateShop(requestModel);
         return response;
-
     }
 
     public async Task<MessageResponseModel> UpdateShop(int id, ShopModel requestModel)
@@ -52,18 +47,22 @@ public class BL_Shop
         {
             throw new Exception("shop is null.");
         }
+
         if (string.IsNullOrWhiteSpace(shop.ShopCode))
         {
             throw new Exception("shop.ShopCode is null.");
         }
+
         if (string.IsNullOrWhiteSpace(shop.ShopName))
         {
             throw new Exception("shop.ShopName is null.");
         }
+
         if (string.IsNullOrWhiteSpace(shop.MobileNo))
         {
             throw new Exception("shop.ShopName is null.");
         }
+
         if (string.IsNullOrWhiteSpace(shop.Address))
         {
             throw new Exception("shop.ShopName is null.");
