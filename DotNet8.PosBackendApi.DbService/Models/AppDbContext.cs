809916861758e73fbe16e742327a1cc5df98af1a
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-05-01 14:30:11 +0630

    -

--- DotNet8.PosBackendApi.DbService/Models/AppDbContext.cs
+++ DotNet8.PosBackendApi.DbService/Models/AppDbContext.cs
@@ -26,11 +26,7 @@ public partial class AppDbContext : DbContext
     public virtual DbSet<TblShop> TblShops { get; set; }
 
     public virtual DbSet<TblStaff> TblStaffs { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=Pos;Integrated Security=True;TrustServerCertificate=True;");
-
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<TblProduct>(entity =>
