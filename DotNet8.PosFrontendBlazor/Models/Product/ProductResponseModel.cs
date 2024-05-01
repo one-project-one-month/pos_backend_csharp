b1393996c00db1ee1070e2016c428e9e540c6f3c
Pyae Phyo Kyaw <pyaephyokyaw.dev@gmail.com>
2024-05-01 21:15:11 +0630

    [Complete] Dialog create

--- DotNet8.PosFrontendBlazor/Models/Product/ProductResponseModel.cs
+++ DotNet8.PosFrontendBlazor/Models/Product/ProductResponseModel.cs
@@ -1,4 +1,4 @@
-﻿namespace DotNet8.PosFrontendBlazor.Models
+﻿namespace DotNet8.PosFrontendBlazor.Models.Product
 {
     public class ProductResponseModel : ResponseModel
     {
---
+++ DotNet8.PosFrontendBlazor/Pages/Product/P_ProductDialog.razor.cs
@@ -0,0 +1,33 @@
+﻿using DotNet8.PosFrontendBlazor.Models.Product;
+
+namespace DotNet8.PosFrontendBlazor.Pages.Product
+{
+    public partial class P_ProductDialog
+    {
+        [CascadingParameter] MudDialogInstance MudDialog { get; set; }
+
+        private ProductRequestModel reqModel = new();
+
+        private void Cancel()
+        {
+            MudDialog.Cancel();
+        }
+
+        private async Task SaveAsync()
+        {
+            var response = await HttpClientService.ExecuteAsync<ProductResponseModel>(
+                Endpoints.ProductCategory,
+                EnumHttpMethod.Post,
+                reqModel
+            );
+            if (response.IsError)
+            {
+                InjectService.ShowMessage(response.Message, EnumResponseType.Error);
+                return;
+            }
+
+            InjectService.ShowMessage(response.Message, EnumResponseType.Success);
+            MudDialog.Close();
+        }
+    }
+}
