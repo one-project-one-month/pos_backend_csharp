a1e0945657b0b893cb45c9cd05a40fab71df6de7
Zin Linn Htet <mackk5504@gmail.com>
2024-05-01 19:28:15 +0630

    Complete Patch Method

--- DotNet8.PosBackendApi/Features/Report/BL_Report.cs
+++ DotNet8.PosBackendApi/Features/Report/BL_Report.cs
@@ -1,15 +1,10 @@
-﻿using DotNet8.PosBackendApi.Models.Setup.Report;
-
-namespace DotNet8.PosBackendApi.Features.Report
+﻿namespace DotNet8.PosBackendApi.Features.Report
 {
     public class BL_Report
     {
         private readonly DL_Report _report;
 
-        public BL_Report(DL_Report report)
-        {
-            _report = report;
-        }
+        public BL_Report(DL_Report report) => _report = report;
 
         public async Task<MonthlyReportResponseModel> MonthlyReport(int month, int year)
         {
