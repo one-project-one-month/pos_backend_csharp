b445ba9aa42f583279c5d6f51f2e5eb0de2d776e
mgchit <chitlunh@gmail.com>
2024-05-12 02:06:26 +0630

    [Inprogress] Staff

--- DotNet8.PosFrontendBlazor/Pages/Staff/P_StaffDialog.razor.cs
+++ DotNet8.PosFrontendBlazor/Pages/Staff/P_StaffDialog.razor.cs
@@ -7,8 +7,36 @@ namespace DotNet8.PosFrontendBlazor.Pages.Staff
     {
         [CascadingParameter] MudDialogInstance MudDialog { get; set; }
 
-        private StaffRequestModel reqModel = new();
+        public StaffRequestModel _reqModel { get; set; } = new();
 
+        [Parameter] public int _staffId { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            await GetStaff();
+        }
+        private async Task GetStaff()
+        {
+            Console.WriteLine(_staffId);
+            if (_staffId > 0)
+            {
+                var staff = await HttpClientService.ExecuteAsync<StaffResponseModel>(
+                    $"{Endpoints.Staff}/ {_staffId}",
+                    EnumHttpMethod.Get
+                    );
+                _reqModel = new StaffRequestModel
+                {
+                    StaffName = staff.Data.Staff.StaffName,
+                    StaffCode = staff.Data.Staff.StaffCode,
+                    Address = staff.Data.Staff.Address,
+                    DateOfBirth = staff.Data.Staff.DateOfBirth,
+                    Gender = staff.Data.Staff.Gender,
+                    MobileNo = staff.Data.Staff.MobileNo,
+                    Password = staff.Data.Staff.Password,
+                    Position = staff.Data.Staff.Position,
+                };
+            }
+        }
         private void Cancel()
         {
             MudDialog.Cancel();
@@ -16,8 +44,8 @@ namespace DotNet8.PosFrontendBlazor.Pages.Staff
 
         private async Task SaveAsync()
         {
-            reqModel.StaffCode = "";
-            DateTime StaffDOB = reqModel.DateOfBirth.ToDateTime();
+            _reqModel.StaffCode = "";
+            DateTime StaffDOB = _reqModel.DateOfBirth.ToDateTime();
             DateTime DateTimeNow = DateTime.Now;
             TimeSpan ageSpan = DateTimeNow - StaffDOB;
             int StaffAge = (int)(ageSpan.Days / 365.25);
@@ -25,26 +53,24 @@ namespace DotNet8.PosFrontendBlazor.Pages.Staff
             {
                 //await JSRuntime.InvokeVoidAsync("alert", "Staff Age must be greater than 18 years.");
                 InjectService.ShowMessage("Staff Age must be greater than 18 years.", EnumResponseType.Success);
+                return;
             }
-            else
-            {
-                var response = await HttpClientService.ExecuteAsync<StaffResponseModel>(
-                Endpoints.Staff,
-                EnumHttpMethod.Post,
-                reqModel
+
+            var response = await HttpClientService.ExecuteAsync<StaffResponseModel>(
+            Endpoints.Staff,
+            EnumHttpMethod.Post,
+            _reqModel
             );
-                if (response.IsError)
-                {
-                    Console.WriteLine(response.Message);
-                    Console.WriteLine(EnumResponseType.Error);
-                    InjectService.ShowMessage(response.Message, EnumResponseType.Error);
-                    return;
-                }
-
-                InjectService.ShowMessage(response.Message, EnumResponseType.Success);
-                MudDialog.Close();
+            if (response.IsError)
+            {
+                Console.WriteLine(response.Message);
+                Console.WriteLine(EnumResponseType.Error);
+                InjectService.ShowMessage(response.Message, EnumResponseType.Error);
+                return;
             }
 
+            InjectService.ShowMessage(response.Message, EnumResponseType.Success);
+            MudDialog.Close();
         }
     }
 }
