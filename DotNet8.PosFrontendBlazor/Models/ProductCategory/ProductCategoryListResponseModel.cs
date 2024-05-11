31269dab09ab4580c9d9da22cfa751dcd4c7059b
Zin Linn Htet <mackk5504@gmail.com>
2024-05-11 21:04:06 +0630

    Change Folder Struvture

---
+++ DotNet8.PosFrontendBlazor/Models/ProductCategory/ProductCategoryListResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DotNet8.PosFrontendBlazor.Models.ProductCategory;
+
+public class ProductCategoryListResponseModel : ResponseModel
+{
+    public ProductCategoryDataModel Data { get; set; }
+    public PageSettingModel PageSetting { get; set; }
+}
