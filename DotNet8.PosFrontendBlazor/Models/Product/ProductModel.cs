a92238c0482b68dc4ae0a832ad5c7ff8b4f6494e
hmwaychocho <hmwaychocho.tze@gmail.com>
2024-05-13 23:11:24 +0630

    -Sale-Invoice(Add New Order)

--- DotNet8.PosFrontendBlazor/Models/Product/ProductModel.cs
+++ DotNet8.PosFrontendBlazor/Models/Product/ProductModel.cs
@@ -7,5 +7,6 @@
         public string? ProductCategoryCode { get; set; } = null!;
         public string? ProductName { get; set; } = null!;
         public decimal Price { get; set; }
+        public int Quantity { get; set; }
     }
 }
