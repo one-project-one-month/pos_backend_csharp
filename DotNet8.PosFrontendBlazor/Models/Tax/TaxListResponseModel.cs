fbe3b5a6bbd984fc243012c091a65d00b8d4ce70
Linn Thit <lth1212001@gmail.com>
2024-05-14 14:52:49 +0630

    -

---
+++ DotNet8.PosFrontendBlazor/Models/Tax/TaxListResponseModel.cs
@@ -0,0 +1,9 @@
+﻿using DotNet8.PosBackendApi.Models;
+
+namespace DotNet8.PosFrontendBlazor.Models.Tax;
+
+public class TaxListResponseModel
+{
+    public List<TaxModel> DataLst { get; set; }
+    public MessageResponseModel MessageResponse { get; set; }
+}
---
+++ DotNet8.PosFrontendBlazor/Pages/Tax/P_Tax.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace DotNet8.PosFrontendBlazor.Pages.Tax
+{
+    public partial class P_Tax
+    {
+        public async Task CreatePopUp()
+        {
+            DialogResult dialogResult = await InjectService.ShowModalBoxAsync<P_CreateTaxDialog>("New Tax");
+        }
+
+        public async Task EditPopUp()
+        {
+            DialogResult dialogResult = await InjectService.ShowModalBoxAsync<P_EditTaxDialog>("Edit Tax");
+        }
+
+        public async Task DeletePopUp()
+        {
+            DialogResult result = await InjectService.ShowModalBoxAsync<P_DeleteTaxDialog>("Delete Tax");
+        }
+    }
+}
