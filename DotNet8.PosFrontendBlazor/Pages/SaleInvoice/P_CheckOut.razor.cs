98e8861ccca688b5c8a340123a3d2c44e0851ea9
hmwaychocho <hmwaychocho.tze@gmail.com>
2024-05-15 21:48:50 +0630

    - Edit SaleInvoice page, Add CheckOut page

---
+++ DotNet8.PosFrontendBlazor/Pages/SaleInvoice/P_CheckOut.razor.cs
@@ -0,0 +1,11 @@
+﻿
+
+using DotNet8.PosFrontendBlazor.Models.SaleInvoice;
+
+namespace DotNet8.PosFrontendBlazor.Pages.SaleInvoice
+{
+    public partial class P_CheckOut
+    {
+        private SaleInvoiceModel? reqModel = new SaleInvoiceModel();
+    }
+}
