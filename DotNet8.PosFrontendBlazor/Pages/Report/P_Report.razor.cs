a26cb9945c72102ecf5e99c002accd88b9a526f7
Pyae Phyo Kyaw <pyaephyokyaw.dev@gmail.com>
2024-05-17 16:23:12 +0630

    Added loading for DDL

--- DotNet8.PosFrontendBlazor/Pages/Report/P_Report.razor.cs
+++ DotNet8.PosFrontendBlazor/Pages/Report/P_Report.razor.cs
@@ -11,33 +11,28 @@ namespace DotNet8.PosFrontendBlazor.Pages.Report
         private DateTime? DateValue { get; set; } = DateTime.Today;
 
         public ReportListResponseModel? responseModel;
-
         private async Task OnValueChanged()
         {
             int? dateMonth = DateValue?.Month;
             int? dateYear = DateValue?.Year;
             if (DateFormat.ToLower() is "monthly")
             {
+                await InjectService.EnableLoading();
                 responseModel = await HttpClientService.ExecuteAsync<ReportListResponseModel>(
                 $"{Endpoints.Report}/monthly-report/{dateMonth}/{dateYear}",
                 EnumHttpMethod.Get
                 );
-                if (responseModel is null)
-                {
-                    Console.WriteLine("");
-                }
             }
             else if (DateFormat.ToLower() is "yearly")
             {
+                await InjectService.EnableLoading();
                 responseModel = await HttpClientService.ExecuteAsync<ReportListResponseModel>(
                 $"{Endpoints.Report}/yearly-report/{dateYear}",
                 EnumHttpMethod.Get
                 );
-                if (responseModel is null)
-                {
-                    Console.WriteLine("");
-                }
             }
+            StateHasChanged();
+            await InjectService.DisableLoading();
         }
     }
 }
