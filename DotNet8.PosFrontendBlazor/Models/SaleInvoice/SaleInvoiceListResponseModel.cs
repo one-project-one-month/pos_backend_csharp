718e9f956a54cfd7b0e630c07712e4d56b2b3d8f
Su Wut Yee <suwutyeenaing@gmail.com>
2024-05-23 16:50:42 +0630

    - Sale Invoice List [Inprogress]

---
+++ DotNet8.PosFrontendBlazor/Models/SaleInvoice/SaleInvoiceListResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNet8.PosFrontendBlazor.Models.SaleInvoice
+{
+    public class SaleInvoiceListResponseModel
+    {
+        public SaleInvoiceDataModel Data { get; set; }
+        public PageSettingModel PageSetting { get; set; }
+    }
+}
