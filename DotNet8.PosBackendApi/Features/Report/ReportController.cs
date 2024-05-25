e363d62e4549e6803cc65135391406d4a38e6452
Hein Htet <ayathar123@gmail.com>
2024-05-26 00:03:36 +0630

    Done API for dashboard

--- DotNet8.PosBackendApi/Features/Report/ReportController.cs
+++ DotNet8.PosBackendApi/Features/Report/ReportController.cs
@@ -191,4 +191,31 @@ public class ReportController : BaseController
             return InternalServerError(ex);
         }
     }
+
+    [HttpPost("dashboard")]
+    public async Task<IActionResult> GetDataForDashboard(DashboardRequestModel Dashboard)
+    {
+        try
+        {
+            var responseModel = await _report.GetDataForDashboard(Dashboard);
+            var model = _response.Return(
+                new ReturnModel
+                {
+                    Token = RefreshToken(),
+                    EnumPos = EnumPos.SaleInvoice,
+                    IsSuccess = responseModel.MessageResponse.IsSuccess,
+                    Message = responseModel.MessageResponse.Message,
+                    BestSellerProduct = responseModel.BestProductData,
+                    DailyData = responseModel.DailyData,
+                    WeeklyData = responseModel.WeeklyData,
+                    MonthlyData = responseModel.MonthlyData,
+                    YearlyData = responseModel.YearlyData
+                });
+            return Content(model);
+        }
+        catch (Exception ex)
+        {
+            return InternalServerError(ex);
+        }
+    }
 }
