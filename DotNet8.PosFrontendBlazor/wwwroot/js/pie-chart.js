f0c658cdaf07ca07b57a8c8cb6d78b6ae28ec48b
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-05-30 00:31:22 +0630

    [Complete] Highchart - Column Chart

--- DotNet8.PosFrontendBlazor/wwwroot/js/pie-chart.js
+++ DotNet8.PosFrontendBlazor/wwwroot/js/pie-chart.js
@@ -78,3 +78,54 @@ window.setLineColumnChart = function (response) {
     var chart = new ApexCharts(document.querySelector("#chart"), options);
     chart.render();
 }
+
+window.setLineChart = function () {
+    Highcharts.chart('container', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Corn vs wheat estimated production for 2020',
+            align: 'left'
+        },
+        subtitle: {
+            text:
+                'Source: <a target="_blank" ' +
+                'href="https://www.indexmundi.com/agriculture/?commodity=corn">indexmundi</a>',
+            align: 'left'
+        },
+        xAxis: {
+            categories: ['USA', 'China', 'Brazil', 'EU', 'India', 'Russia'],
+            crosshair: true,
+            accessibility: {
+                description: 'Countries'
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: '1000 metric tons (MT)'
+            }
+        },
+        tooltip: {
+            valueSuffix: ' (1000 MT)'
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [
+            {
+                name: 'Corn',
+                data: [406292, 260000, 107000, 68300, 27500, 14500]
+            },
+            {
+                name: 'Wheat',
+                data: [51086, 136000, 5500, 141000, 107180, 77000]
+            }
+        ]
+    });
+
+}
