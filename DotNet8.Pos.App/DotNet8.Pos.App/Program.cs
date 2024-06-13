ba22c372c620dc9ec2c61f11d7b1cd3dcbdb3c80
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-06-13 18:58:00 +0630

    [Complete] Remove Blazor Web Assembly

--- DotNet8.Pos.App/DotNet8.Pos.App/Program.cs
+++ DotNet8.Pos.App/DotNet8.Pos.App/Program.cs
@@ -1,13 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents()
-    .AddInteractiveWebAssemblyComponents();
-
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-    //.AddInteractiveWebAssemblyComponents();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7164") });
 builder.Services.AddScoped<HttpClientService>();
@@ -18,7 +12,6 @@ builder.Services.AddRadzenComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
@@ -26,7 +19,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -34,12 +26,6 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
-
-
-//app.MapRazorComponents<App>()
-//    .AddInteractiveServerRenderMode()
-//    .AddInteractiveWebAssemblyRenderMode()
-//    .AddAdditionalAssemblies(typeof(DotNet8.Pos.App.Client._Imports).Assembly);
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
