591e9dbe74163738b7338055f2e9f3aa21648baf
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-08-05 17:09:28 +0630

    feat: add swagger

--- DotNet8.PosBackendApi/Program.cs
+++ DotNet8.PosBackendApi/Program.cs
@@ -45,11 +45,14 @@ builder.AddJwtAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
